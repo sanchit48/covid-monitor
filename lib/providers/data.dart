f76b54ea80965d146a88e016034ae2355eabbe75
sanchit48 <sanchitkhare3@gmail.com>
2020-05-17 00:44:40 +0530

    data.dart: Added global data and country wise data classes

--- lib/providers/data.dart
+++ lib/providers/data.dart
@@ -1,11 +1,11 @@
 import 'dart:convert';
-
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 
-class Data with ChangeNotifier {
+class GlobalData with ChangeNotifier {
 
   final String id;
+  final int totalCases;
   final int newConfirmed;
   final int totalConfirmed;
   final int newDeaths;
@@ -13,8 +13,31 @@ class Data with ChangeNotifier {
   final int newRecovered;
   final int totalRecovered;
 
-  Data({
+  GlobalData({
     @required this.id,
+    @required this.totalCases,
+    @required this.newConfirmed,
+    @required this.totalConfirmed,
+    @required this.newDeaths,
+    @required this.totalDeaths,
+    @required this.newRecovered,
+    @required this.totalRecovered
+  });
+
+}
+
+class CountryWiseData with ChangeNotifier {
+
+  final String country;
+  final int newConfirmed;
+  final int totalConfirmed;
+  final int newDeaths;
+  final int totalDeaths;
+  final int newRecovered;
+  final int totalRecovered;
+
+  CountryWiseData({
+    @required this.country,
     @required this.newConfirmed,
     @required this.totalConfirmed,
     @required this.newDeaths,
