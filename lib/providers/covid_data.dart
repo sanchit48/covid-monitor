e6042b6488fa82b3a578d06c6a2d128f0c03649a
sanchit48 <sanchitkhare3@gmail.com>
2020-05-17 00:43:53 +0530

    covid_data.dart: Added classes of covid data

--- lib/providers/covid_data.dart
+++ lib/providers/covid_data.dart
@@ -6,36 +6,151 @@ import 'data.dart';
 
 class CovidData with ChangeNotifier {
 
+  List<String> _globalList = [
 
-  Future<void> fetchCovidData() async {
+    'newConfirmed',
+    'totalConfirmed',
+    'newDeaths',
+    'totalDeaths',
+    'newRecovered',
+    'totalRecovered'
 
-    const url = 'https://api.covid19api.com/summary';
-    try {
+  ];
 
-      final response =  await http.get(url);
-      final extractedData = json.decode(response.body) as Map<String, dynamic>;
-      print(response.body);
-      if(extractedData == null)
-        return;
-      final List<Data> loadedData = [];
-      Map<String, int> map = extractedData['Global'];
+  Map<String, dynamic> _globalMap = {
 
-      loadedData.add(
-        Data(
-          id: 'Global',
-          newConfirmed: map['NewConfirmed'],
-          totalConfirmed: map['TotalConfirmed'],
-          newDeaths: map['NewDeaths'],
-          totalDeaths: map['TotalDeaths'],
-          newRecovered: map['NewRecovered'],
-          totalRecovered: map['TotalRecovered'],
-      ));
+    'newConfirmed': 10000,
+    'totalConfirmed': 10000,
+    'newDeaths': 10000,
+    'totalDeaths': 10000,
+    'newRecovered': 10000,
+    'totalRecovered': 10000,
 
-    }
-    catch (error) {
-      throw (error);
-    }
+  };
 
-  }
+  var _globalData = GlobalData(
+
+    totalCases: 1000000,
+    totalDeaths: 1000000,
+    totalRecovered: 1000000,
+
+  );
+
+   GlobalData get globalData {
+     return _globalData;
+   }
+
+   List<CountryWiseData> _countryWiseList = [
+
+     CountryWiseData(
+       country: 'India',
+       newConfirmed: 10000,
+       totalConfirmed: 10000,
+       newDeaths: 10000,
+       totalDeaths: 10000,
+       newRecovered: 10000,
+       totalRecovered: 10000,
+     ),
+
+     CountryWiseData(
+       country: 'India',
+       newConfirmed: 10000,
+       totalConfirmed: 10000,
+       newDeaths: 10000,
+       totalDeaths: 10000,
+       newRecovered: 10000,
+       totalRecovered: 10000,
+     ),
+
+     CountryWiseData(
+       country: 'India',
+       newConfirmed: 10000,
+       totalConfirmed: 10000,
+       newDeaths: 10000,
+       totalDeaths: 10000,
+       newRecovered: 10000,
+       totalRecovered: 10000,
+     ),
+
+     CountryWiseData(
+       country: 'India',
+       newConfirmed: 10000,
+       totalConfirmed: 10000,
+       newDeaths: 10000,
+       totalDeaths: 10000,
+       newRecovered: 10000,
+       totalRecovered: 10000,
+     ),
+
+     CountryWiseData(
+       country: 'India',
+       newConfirmed: 10000,
+       totalConfirmed: 10000,
+       newDeaths: 10000,
+       totalDeaths: 10000,
+       newRecovered: 10000,
+       totalRecovered: 10000,
+     ),
+
+     CountryWiseData(
+       country: 'India',
+       newConfirmed: 10000,
+       totalConfirmed: 10000,
+       newDeaths: 10000,
+       totalDeaths: 10000,
+       newRecovered: 10000,
+       totalRecovered: 10000,
+     ),
+
+     CountryWiseData(
+       country: 'India',
+       newConfirmed: 10000,
+       totalConfirmed: 10000,
+       newDeaths: 10000,
+       totalDeaths: 10000,
+       newRecovered: 10000,
+       totalRecovered: 10000,
+     ),
+   ];
+
+   List<CountryWiseData> get countryWiseList {
+     return _countryWiseList;
+   }
+
+   Map<String, dynamic> get globalMap {
+     return _globalMap;
+   }
+
+   List<String> get globalList {
+     return _globalList;
+   }
+
+  // Future<void> fetchCovidData() async {
+  //   const url = 'https://api.covid19api.com/summary';
+  //   try {
+  //     final response =  await http.get(url);
+  //     final extractedData = json.decode(response.body) as Map<String, dynamic>;
+  //     if(extractedData == null)
+  //       return;
+  //     final List<Data> loadedData = [];
+  //     Map<String, dynamic> map = extractedData['Global'];
+  //    // print(map);
+  //     loadedData.add(
+  //       Data(
+  //         id: 'Global',
+  //         newConfirmed: map['NewConfirmed'],
+  //         totalConfirmed: map['TotalConfirmed'],
+  //         newDeaths: map['NewDeaths'],
+  //         totalDeaths: map['TotalDeaths'],
+  //         newRecovered: map['NewRecovered'],
+  //         totalRecovered: map['TotalRecovered'],
+  //     ));
+  //     //print(loadedData[0]);
+  //     notifyListeners();
+  //   }
+  //   catch (error) {
+  //     throw (error);
+  //   }
+  // }
 
 }
