89bc5fe197f29f16831fe17e9e7ea622120bb349
sanchit48 <sanchitkhare3@gmail.com>
2020-05-21 23:14:14 +0530

    helpline_item.dart: This shows helpline numbers

--- lib/widgets/helpline_item.dart
+++ lib/widgets/helpline_item.dart
@@ -1,7 +1,12 @@
 import 'package:covid_tracker/helpers/sizes.dart';
+import 'package:covid_tracker/providers/data.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 class HelplineItem extends StatelessWidget {
+
+  final Helpline helpline;
+  HelplineItem(this.helpline);
+
   @override
   Widget build(BuildContext context) {
 
@@ -9,39 +14,42 @@ class HelplineItem extends StatelessWidget {
     var deviceWidth = getViewportWidth(context);
 
     return Card(
+      elevation: 5,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(15.0),
-
       ),
-      //color: Color(0xff1b5e20),
       child: ListTile(
 
         title: Padding(
           padding: const EdgeInsets.only(top: 6),
           child: Text(
-            'Uttar Pradesh',
+            helpline.stateName,
             style: TextStyle(
-              fontSize: deviceHeight*0.03,
-              fontWeight: FontWeight.bold
+              fontSize: deviceHeight*0.028,
             ),
+            overflow: TextOverflow.ellipsis,
           ),
         ),
-        subtitle: Text(
-          '18001805145',
-          style: TextStyle(
-            fontSize: deviceHeight*0.022,
-            fontWeight: FontWeight.bold
+        subtitle: Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: Text(
+            helpline.helplineNumber,
+            style: TextStyle(
+              fontSize: deviceHeight*0.022,
+              fontWeight: FontWeight.bold
+            ),
           ),
         ),
         trailing: Container(
           margin: EdgeInsets.only(right: 11),
           child: IconButton(
             icon: Icon(Icons.phone, size: deviceHeight*0.04,color: Color(0xff1b5e20),),
-            onPressed: () => launch("tel:8958464190"),
+            onPressed: () => launch("tel:${helpline.helplineNumber}"),
           )
         ),
 
       ),
+
     );
   }
 }
