5799ef6c8d2167dd6a9e2e85f998f50c4977b439
sanchit48 <sanchitkhare3@gmail.com>
2020-05-21 23:13:46 +0530

    country_data_item.dart: This shows real covid data of countries

--- lib/widgets/country_data_item.dart
+++ lib/widgets/country_data_item.dart
@@ -4,10 +4,9 @@ import 'package:flutter/material.dart';
 
 class CountryDataItem extends StatelessWidget {
 
-  final CountryWiseData countryWiseData;
+  final CountryData countryData;
 
-  CountryDataItem(this.countryWiseData);
-
+  CountryDataItem(this.countryData);
 
   @override
   Widget build(BuildContext context) {
@@ -16,16 +15,14 @@ class CountryDataItem extends StatelessWidget {
     var deviceWidth = getViewportWidth(context);
 
     return InkWell(
-        child: Card(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(15),
-          ),
-          elevation: 5,
-         margin: EdgeInsets.all(27),
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+        elevation: 5,
+        margin: EdgeInsets.all(27),
           child: Column(
             children: <Widget>[
-
-              // force the child into certain form
               ClipRRect(
                 borderRadius: BorderRadius.only(
                   topLeft: Radius.circular(15),
@@ -33,43 +30,41 @@ class CountryDataItem extends StatelessWidget {
                 ),
                 child: Image.asset(
                   'assets/images/newsTest.jpg',
-                   height: deviceHeight*0.17,
-                   width: double.infinity,
+                  height: deviceHeight*0.17,
+                  width: double.infinity,
                   fit: BoxFit.cover,
                 ),
               ),
 
-              Container(
-                height: deviceHeight*0.05,
-                width: deviceWidth*0.4,
-              //  padding: EdgeInsets.all(20),
-                decoration: BoxDecoration(
+            Container(
+              height: deviceHeight*0.05,
+              width: deviceWidth*0.4,
+              decoration: BoxDecoration(
 
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(15),
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(15),
 
-                ),
-                child: Center(
+              ),
+              child: Center(
 
-                  child:  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: FittedBox(
-                      child: Text(
-                        countryWiseData.country,
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: deviceHeight*0.026,
-                          fontWeight: FontWeight.bold,
-                          fontFamily: 'Montserrat',
-                        ),
+                child:  Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: FittedBox(
+                    child: Text(
+                      countryData.country,
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: deviceHeight*0.026,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Montserrat',
                       ),
                     ),
                   ),
                 ),
-              )
-            ],
-          ),
-
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
