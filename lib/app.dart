33f0e20736ef40c869fef939b99bf6f45e9b5ebb
sanchit48 <sanchitkhare3@gmail.com>
2020-05-31 23:35:13 +0530

    app.dart: Added about screen route

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:covid_tracker/providers/covid_data.dart';
 import 'package:covid_tracker/providers/helpline.dart';
+import 'package:covid_tracker/screens/about_screen.dart';
 import 'package:covid_tracker/screens/country_screen.dart';
 import 'package:covid_tracker/screens/country_wise_data_screen.dart';
 import 'package:covid_tracker/screens/helpline_screen.dart';
@@ -7,6 +8,7 @@ import 'package:covid_tracker/widgets/navigation_bar.dart';
 import 'package:covid_tracker/screens/global_data_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
 class CovidMonitor extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -25,13 +27,12 @@ class CovidMonitor extends StatelessWidget {
         title: 'shareApp',
         color: Color(0xff1b5e20),
         routes: {
-
           '/': (ctx) => NavigationBar(),
           GlobalDataScreen.routeName: (ctx) => GlobalDataScreen(),
           CountryWiseDataScreen.routeName: (ctx) => CountryWiseDataScreen(),
           CountryScreen.routeName: (ctx) => CountryScreen(),
           HelplineScreen.routeName: (ctx) => HelplineScreen(),
-
+          AboutScreen.routeName: (ctx) =>  AboutScreen(),
         },
       ),
 
